c1378396f94545be9a645371b283227f336aca29
manhtranduc <manhtranduc0202@gmail.com>
2025-06-03 13:57:23 +0700

    dùng template email

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Resend } from 'resend';
 import envConfig from 'src/shared/config';
-
+import fs from 'fs';
+import path from 'path';
 @Injectable()
 export class EmailService {
   private resend: Resend;
@@ -9,11 +10,20 @@ export class EmailService {
     this.resend = new Resend(envConfig.RESEND_API_KEY);
   }
   sendOTP(payload: { email: string; code: string }) {
+    const otpTemplate = fs.readFileSync(
+      path.resolve('src/shared/email-templates/otp.html'),
+      {
+        encoding: 'utf-8',
+      },
+    );
+
     return this.resend.emails.send({
       from: 'Ecommerce <onboarding@resend.dev>',
       to: ['manhtranduc0202@gmail.com'], // sandbox chỉ dùng được cái này thôi
       subject: 'Verify your email address',
-      html: `<strong>Your verification code is: ${payload.code}.</strong>`,
+      html: otpTemplate
+        .replaceAll('{{code}}', payload.code)
+        .replaceAll('{{email}}', payload.email),
     });
   }
 }
