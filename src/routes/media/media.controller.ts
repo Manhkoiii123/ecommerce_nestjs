133228ff117234f67da34eeec45e31b3797531cd
manhtranduc <manhtranduc0202@gmail.com>
2025-06-10 22:02:18 +0700

    File validation

--- src/routes/media/media.controller.ts
+++ src/routes/media/media.controller.ts
@@ -1,5 +1,8 @@
 import {
   Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -9,8 +12,24 @@ import { FileInterceptor } from '@nestjs/platform-express';
 @Controller('media')
 export class MediaController {
   @Post('images/upload')
-  @UseInterceptors(FileInterceptor('file')) // key là file trên formData
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: {
+        fileSize: 1024 * 1024 * 10, // ưu tiên lấy cái này
+      },
+    }),
+  ) // key là file trên formData
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 10 }),
+          new FileTypeValidator({ fileType: /(jpeg|jpg|png|webp)$/ }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     console.log(file);
   }
 }
