3eb48da27715f4d4f5ababb2477c96af0bd06a09
manhtranduc <manhtranduc0202@gmail.com>
2025-06-11 10:01:27 +0700

    kết nối s3

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { ApiKeyGuard } from 'src/shared/guards/apiKey.guard';
 import { SharedUserRepository } from 'src/shared/repositories/shared-user.repo';
 import { EmailService } from 'src/shared/services/email.service';
 import { TwoFactorAuthService } from 'src/shared/services/2fa.service';
+import { S3Service } from 'src/shared/services/s3.service';
 const sharedServices = [
   PrismaService,
   HashingService,
@@ -17,6 +18,7 @@ const sharedServices = [
   SharedUserRepository,
   EmailService,
   TwoFactorAuthService,
+  S3Service,
 ];
 @Global()
 @Module({
