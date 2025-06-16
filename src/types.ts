969ccc366b4ee807673e9924b4d15dde673c387e
manhtranduc <manhtranduc0202@gmail.com>
2025-06-16 14:34:01 +0700

    fix schema order

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,19 @@
 /* eslint-disable @typescript-eslint/no-namespace */
+import { ProductTranslationType } from 'src/routes/product/product-translation/product-translation.model';
 import { VariantsType } from 'src/routes/product/product.model';
 
 declare global {
   namespace PrismaJson {
     type Variants = VariantsType;
+    type ProductTranslations = Pick<
+      ProductTranslationType,
+      'id' | 'name' | 'description' | 'languageId'
+    >[];
+    type Receiver = {
+      name: string;
+      phone: string;
+      address: string;
+    };
   }
 }
 
